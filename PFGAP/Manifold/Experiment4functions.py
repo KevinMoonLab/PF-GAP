0390f7b334448e53fb3af161abda8f3f6542bfb1
rustadadam <rustadadam@gmail.com>
2025-08-15 17:46:41 -0600

    Functionalized part 1

---
+++ PFGAP/Manifold/Experiment4functions.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+import geomstats.backend as gs
+import geomstats.visualization as visualization
+from geomstats.geometry.hypersphere import Hypersphere
+from geomstats.geometry.special_orthogonal import SpecialOrthogonal
+
+# Code for Experiment 4 functions
+
+#& Data Constants
+SEEDS = [61, 737, 821, 161, 346, 78, 2, 67, 102, 982]
+
+#& Generate Data Functions
+def create_3d_sphere_data(seed):
+    """Creates two clusters on a 3d sphere"""
+
+    np.random.seed(seed)
+    gs.random.seed(seed)
+
+    sphere = Hypersphere(dim=2, equip=False)
+    cluster = sphere.random_von_mises_fisher(kappa=20, n_samples=150)
+
+    SO3 = SpecialOrthogonal(3, equip=False)
+    rotation1 = SO3.random_uniform()
+    rotation2 = SO3.random_uniform()
+
+    cluster_1 = cluster @ rotation1
+    cluster_2 = cluster @ rotation2
+
+    #Create labels
+    labels_1 = np.zeros(cluster_1.shape[0])
+    labels_2 = np.ones(cluster_2.shape[0])
+    labels = np.concatenate([labels_1, labels_2])
+
+    #Combine data
+    data = np.concatenate([cluster_1, cluster_2], axis=0)
+    
+    return data, labels
